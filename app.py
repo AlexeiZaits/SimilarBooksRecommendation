33b2c0d619d1336b73b1d62b9517a1dbd876b566
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-07-26 01:28:44 +0300

    add: init fastapi wrapper for qdrant

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.router import router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(App: FastAPI):
+    """Запуск загрузки модели и эмбеддера в фоновом режиме при старте приложения"""
+    # TODO: реализовать фоновый запуск
+    yield
+    # Здесь можно добавить действия при завершении работы приложения
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router, prefix="/api")
 
--- src/router.py
+++ src/router.py
@@ -1,21 +1,47 @@
+import os
 from http import HTTPStatus
+from typing import Annotated
 
-import joblib
-from fastapi import APIRouter
+from dotenv import load_dotenv
+from fastapi import APIRouter, Body, Depends
 
-from src.faiss_scripts import inference, load_df
-from src.schemas import BooksResponse
+from src.models import get_embedder, get_qdrant_connection
+from src.schemas import BooksResponse, DescriptionInput
+
+load_dotenv()
 
 router = APIRouter(tags=["ML API"])
-df = load_df()
-vector_db = joblib.load("models/vector_db.pkl")
 
 
-# Запрос для предикта Any = Body(None)
-@router.post("/inference", response_model=BooksResponse)  #
-def predict(book_desc: str):
+ROOT_PATH = os.getcwd()
+
+
+@router.post("/predict", response_model=BooksResponse)
+def predict(
+    data: Annotated[DescriptionInput, Body()], _qdrant=Depends(get_qdrant_connection), _embedder=Depends(get_embedder)
+):
     """Метод для получения инференса по api"""
-    if vector_db:
-        result = inference(df, book_desc, vector_db, n_chunks=6)
-        print(result)
-    return {"status": HTTPStatus.OK, "data": result}
+
+    # Проверяем наличие данных
+    if not data.book_desc:
+        return BooksResponse(status=HTTPStatus.BAD_REQUEST, data=[], error="Input description is not found")
+
+    # TODO: передавать не описание, а название книги. Продумать как это можно сделать
+    # Мб это отдельная коллекция с названиями книг -> искать по ним наиболее подходящее и по нему брать описание
+
+    # TODO: Реализовать обработку ошибок
+    # Если данные есть, то получаем предикт
+    # try:
+    query_vector = _embedder.encode(data.book_desc, batch_size=1, normalize_embeddings=True)
+    message_content = _qdrant.search(
+        collection_name=data.collection_name, query_vector=query_vector, limit=data.n_chunks, offset=data.offset
+    )
+
+    # TODO: Реализовать поиск по датасету с книгами. Например Redis
+    result = [dict(recommendation.payload, score=recommendation.score) for recommendation in message_content]
+    # # Отлавливаем ошибки и возвращаем их на бэк
+    # except Exception as err:
+    #     return BooksResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR, data=[], error=err)
+
+    # Если ошибок не было
+    return BooksResponse(status=HTTPStatus.OK, data=result, error="")
