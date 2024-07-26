6762fafe4d5cc42875b341224d6a6804c4b3b575
Алексей <zaykuh@gmail.com>
2024-07-26 19:08:05 +0500

    fix logic for button

--- frontend/src/shared/ui/button/ui/Button.tsx
+++ frontend/src/shared/ui/button/ui/Button.tsx
@@ -1,22 +1,29 @@
 import { CSSProperties, ReactNode } from "react"
 import styles from "./styles.module.scss"
+import classNames from "classnames"
 
 interface IButton {
     text?: string,
     svg?: ReactNode,
     onClick?: () => void,
-    style: CSSProperties,
+    style?: CSSProperties,
+    secondary?: boolean,
     children?: ReactNode,
 }
 
-export const Button = ({text, svg, children, onClick, ...otherProps}:IButton) => {
-
+export const Button = ({text, svg, secondary, children, onClick, ...otherProps}:IButton) => {
+    
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
         onClick && onClick()
     }
-        
-    return <button className={styles.button} onClick={handleClick} {...otherProps}>
+
+    return <button className={classNames(
+        styles.button,
+        {[styles.secondary]: secondary},
+    )} 
+    onClick={handleClick} 
+    {...otherProps}>
         {svg}
         {text}
         {children}
