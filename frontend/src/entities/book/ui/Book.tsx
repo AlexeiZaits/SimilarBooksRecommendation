796fa3daaa5eb368c12a0c90a000cb62ecb7d3ba
Алексей <zaykuh@gmail.com>
2024-07-26 19:05:20 +0500

    fix book

--- frontend/src/entities/book/ui/Book.tsx
+++ frontend/src/entities/book/ui/Book.tsx
@@ -1,13 +1,20 @@
 import { IBook } from "shared/types/book"
 import styles from "./styles.module.scss";
-import img from "../../../shared/assets/icons/54300.jpg"
+import { imgBook } from "../lib/imgBase64";
+import { CSSProperties, ReactNode } from "react";
 
-export const Book = ({author, title}: Pick<IBook, "author" | "title"> ) => {
-    return <div className={styles.book}>
-        <div className={styles.container}><img className={styles.img} src={img} alt="img"/></div>
+export interface BooKProps extends Pick<IBook, "author" | "title">  {
+    children?: ReactNode,
+    style?: CSSProperties,
+}
+
+export const Book = ({author, title, children, ...otherProps}: BooKProps ) => {
+    return <div {...otherProps} className={styles.book}>
+        {children}
+        <div className={styles.container}><img className={styles.img} src={`data:image/jpeg;base64,${imgBook}`} alt="img"/></div>
         <div className={styles.info}>
-            <span className={styles.author}>{author}</span>
             <span className={styles.title}>{title}</span>
+            <span className={styles.author}>{author}</span>
         </div>
     </div>
 }
