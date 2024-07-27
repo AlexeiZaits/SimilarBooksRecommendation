fb524406104ccee561addfa049be2e7dece44f3c
Алексей <zaykuh@gmail.com>
2024-07-27 15:25:17 +0500

    fix: fix post request for recommendListSlice/search

--- frontend/src/features/recommendList/model/recommend-list-actions.ts
+++ frontend/src/features/recommendList/model/recommend-list-actions.ts
@@ -1,22 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Extra } from "shared/types";
-import $client from "shared/api";
+// import $client from "shared/api";
 import { IBook } from "shared/types/book";
+import axios from "axios";
 
 export const actionSearchBooks = createAsyncThunk<
     Omit<IBook, "description">[],
     string,
-{ 
+{
     extra: Extra,
     rejectValue: string,
 }
 >(
     '@@recommendListSlice/search',
-    async(search, 
+    async(search,
         { extra: {api}, rejectWithValue,
     }) => {
         try {
-            return $client.post(api.searchBooks(search));
+            return axios.post(api.searchBooks, {
+              "book_desc": search,
+              "n_chunks": 6,
+              "offset": 0,
+              "collection_name": "SimilarBooksService"
+            });
           } catch (error) {
             if (error instanceof Error)
               return rejectWithValue(error.message);
