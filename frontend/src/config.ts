c701cc07b78a1445cf06a60be52d30d20a8e4b11
Алексей <zaykuh@gmail.com>
2024-07-27 15:24:07 +0500

    fix: fix api_url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
-export const API_URL = 'https://backend/api/';
+export const API_URL = 'http://127.0.0.1:8000/api/';
 
 export const authUser = API_URL + `login`
 export const logout = API_URL + `logout`
 export const resresh = API_URL + `resresh`
-export const searchBooks = (search:string) => API_URL + `search?${search}`
+export const searchBooks = API_URL + `predict`
