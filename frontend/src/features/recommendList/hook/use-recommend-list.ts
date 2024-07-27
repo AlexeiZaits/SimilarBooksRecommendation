b37d91b45699b0c145646f7f64125a7e51b2e2bd
Алексей <zaykuh@gmail.com>
2024-07-27 15:26:49 +0500

    refactor

--- frontend/src/features/recommendList/hook/use-recommend-list.ts
+++ frontend/src/features/recommendList/hook/use-recommend-list.ts
@@ -6,12 +6,12 @@ import { actionSearchBooks } from "../model/recommend-list-actions"
 export const useRecommendList = (): [
     ReturnType<typeof selectRecommendList>,
     (search: string) => void
-] => 
-{   
+] =>
+{
     const dispatch = useAppDispatch()
 
     const booksInfo = useAppSelector(selectRecommendList)
     const searchBooks = (search: string) => dispatch(actionSearchBooks(search))
-    
+
     return [{...booksInfo}, searchBooks]
 }
