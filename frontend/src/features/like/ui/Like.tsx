e04866b72157fc47a9dd1c975fca8020cc5fb0bd
Алексей <zaykuh@gmail.com>
2024-07-27 17:24:13 +0500

    feat: fix like

--- frontend/src/features/like/ui/Like.tsx
+++ frontend/src/features/like/ui/Like.tsx
@@ -3,13 +3,14 @@ import styles from "./styles.module.scss";
 import { LikeImg } from "shared/assets/icons/LikeImg";
 import { IBook } from "shared/types/book";
 
-export const Like = ({author, id, title, image}: Pick<IBook, "author" | "id" | "title" | "image">) => {
+export const Like = ({ id }: Pick<IBook, "id">) => {
     const [like, setLike] = useState(false)
 
     useEffect(()=> {
-        like ? localStorage.setItem(`book?${id}`, JSON.stringify({author: author, title:title, image:image})) : localStorage.removeItem(`book?${id}`)
+        like ? localStorage.setItem(`book?${id}`, JSON.stringify({id: id})) : localStorage.removeItem(`book?${id}`)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [like])
-    
+
     return <div className={styles.like} onClick={() => setLike((prevState) => !prevState)}>
         <LikeImg like={like}/>
     </div>
