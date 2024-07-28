27947676663bb52b6bc08974089488f5f4faa6c2
Алексей <zaykuh@gmail.com>
2024-07-28 14:00:42 +0500

    store: add searchSlice, infinityScrollSlice

--- frontend/src/app/store/store.ts
+++ frontend/src/app/store/store.ts
@@ -4,11 +4,15 @@ import axios from 'axios'
 import { authSlice } from 'features/authorization/model/auth-slice';
 import * as api from "../../config"
 import { recommendListSlice } from 'features/recommendList/model/recommend-list-slice';
+import { searchSlice } from 'features/search/model/search-slice';
+import { infinityScrollSlice } from 'features/infinityScroll/model/infinity-scroll-slice';
 
 export const store = configureStore({
   reducer: {
     authorization: authSlice.reducer,
     recommendList: recommendListSlice.reducer,
+    searchRecommend: searchSlice.reducer,
+    infinityScroll: infinityScrollSlice.reducer,
   },
   devTools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
@@ -26,4 +30,3 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
 export const useAppSelector = useSelector.withTypes<RootState>()
 export const useAppDispatch: () => AppDispatch = useDispatch;
-    
