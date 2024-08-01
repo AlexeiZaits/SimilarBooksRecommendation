eea80739bab282f93a9c8452cd487a324e7c28f3
Алексей <zaykuh@gmail.com>
2024-08-01 20:52:37 +0500

    config: update

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,7 @@
 export const API_URL = 'http://127.0.0.1:8000/api/';
 
-export const authUser = API_URL + `login`
-export const logout = API_URL + `logout`
-export const resresh = API_URL + `resresh`
-export const searchBooks = API_URL + `predict`
+export const authUser = API_URL + `login`;
+export const logout = API_URL + `logout`;
+export const resresh = API_URL + `resresh`;
+export const searchBooks = API_URL + `get_book_recommendations`;
+export const get_titles_books = API_URL + `update_query`;
