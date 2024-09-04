7c4f44b9ebe0b0ab6f17ca1da21d30e5bdeb34b5
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-09-04 17:52:56 +0300

    upd: update config

--- backend/config/custom_status.py
+++ backend/config/custom_status.py
@@ -6,6 +6,8 @@ class CustomHTTPStatus(int, Enum):
 
     UnknownExceptionStatus = 1000  # Неизвестная ошибка
     RedisDataNotFouldStatus = 1001  # Данные не найдены в Redis по входящему запросу
-    RankingErrorStatus = 1002  # Не найдены похожие книги
+    RankingErrorStatus = 1002  # Ошибка во время ранжирования
     AutocompliteIsEmptyStatus = 1003  # Не найдены походие книги в автокомплите
     QdrantSearchError = 1004  # Ошибка в поиске Qdrant или в построении эмбеддингов
+    DataBaseError = 1005  # Ошибка при запросе к базе данных
+    CategoryFilterIsNotFound = 1006  # Введенный в запрос фильтр отсутствует в данных
