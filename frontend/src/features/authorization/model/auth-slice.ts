d68341963b3a1d8d80dd2898d86d37f82902c74b
Алексей <zaykuh@gmail.com>
2024-10-13 13:35:58 +0500

    upd: feature Authorization

--- frontend/src/features/authorization/model/auth-slice.ts
+++ frontend/src/features/authorization/model/auth-slice.ts
@@ -1,28 +1,54 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import {IUser} from "shared/types";
 import { ErrorType, Status } from "shared/types/status";
 import { authUser, logoutUser, refreshUser, regUser } from "./auth-actions";
+import { updateObject } from "widjets/authForm/lib/updateObjetc";
 
 export interface IAuthSlice{
   status: Status,
   error: ErrorType,
   user: IUser|null,
-  isAuth: boolean
+  isAuth: boolean,
+  message: string,
+  name: string,
 }
+
+type IData = Status | ErrorType | IUser | null | boolean | string
 
 export const initialState:IAuthSlice = {
   status: "idle",
   error: null,
   user: null,
-  isAuth: false
+  isAuth: false,
+  message: "",
+  name: "",
+}
+
+export type TKeyAuth = keyof IAuthSlice
+
+export type TAuthPayload = {
+  [K in TKeyAuth]: IAuthSlice[K];
+};
+
+
+export interface ActionAuthSlice {
+  key: TKeyAuth,
+  data: TAuthPayload[TKeyAuth]
 }
 
 export const authSlice = createSlice({
     name: "@authSlice",
     initialState: initialState,
     reducers: {
+      setAuthSlice: (state, action: PayloadAction<ActionAuthSlice>) => {
+        const {key, data} = action.payload
+        state = updateObject<IAuthSlice, IData>(state, key, data)
+      },
       clearErrorAuth: (state) => {
         state.error = null
+      },
+      clearStatusAuth: (state) => {
+        state.status = "idle"
       }
     },
     extraReducers: (builder) => {
@@ -31,9 +57,10 @@ export const authSlice = createSlice({
           state.status = 'loading';
           state.error = null;
         })
-        .addCase(authUser.rejected, (state, action) => {
+        .addCase(authUser.rejected, (state, {payload}) => {
           state.status = 'rejected';
-          state.error = action.payload || 'Cannot load data';
+          state.error = payload || 'Cannot load data';
+          state.message = String(payload)
         })
         .addCase(authUser.fulfilled, (state, {payload}) => {
           localStorage.setItem("token", payload.data.access_token)
@@ -52,27 +79,30 @@ export const authSlice = createSlice({
         .addCase(refreshUser.fulfilled, (state, {payload}) => {
           localStorage.setItem("token", payload.data.access_token)
           state.status = 'received';
-          state.user = payload.data.user;
+          // state.user = payload.data.user;
+          state.name = payload.data.login;
           state.isAuth = true;
         })
         .addCase(logoutUser.fulfilled, (state,) => {
           localStorage.removeItem("token")
           state.isAuth = false;
           state.user = null;
+          state.name = ""
         })
         .addCase(regUser.pending, (state) => {
           state.status = 'loading';
           state.error = null;
         })
-        .addCase(regUser.rejected, (state, action) => {
+        .addCase(regUser.rejected, (state, {payload}) => {
           state.status = 'rejected';
-          state.error = action.payload || 'Cannot load data';
+          state.error = payload || 'Cannot load data';
+          state.message = payload || 'Ошибка при регистрации';
         })
         .addCase(regUser.fulfilled, (state, {payload}) => {
-          console.log(payload.data.message)
+          state.message = payload.data.message;
           state.status = 'received';
         })
     }
 })
 
-export const {clearErrorAuth} =  authSlice.actions
+export const {clearErrorAuth, clearStatusAuth, setAuthSlice} =  authSlice.actions
