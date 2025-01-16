74ab6d64234b04224f82b81014b43fff03c9b658
Алексей <bancher@MacBook-Air-Aleksej.local>
2025-01-16 23:36:54 +0500

    fix: indexHtml, cardAnime,CollabsibleDescription

--- frontend/src/features/collapsibleDescription/ui/CollabsibleDescription.tsx
+++ frontend/src/features/collapsibleDescription/ui/CollabsibleDescription.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.scss"
 import classNames from "classnames";
 
@@ -9,21 +9,47 @@ interface ICollabsibleDescription{
 export const CollabsibleDescription = ({description}: ICollabsibleDescription) => {
 
     const [isExpanded, setIsExpanded] = useState(false);
+    const accordionBodyRef = useRef<HTMLDivElement>(null)
+    const [height, setHeigth] = useState(0)
 
     const toggleDescription = () => {
         setIsExpanded(!isExpanded);
     };
 
+    const [wrapperHeight, setWrapperHeight] = useState("auto");
+
+    useEffect(() => {
+        const accordionBody = accordionBodyRef.current;
+
+        if (!accordionBody) return;
+        
+        const updateWrapperHeight = () => {
+            if (!isExpanded){
+                setTimeout(() => setWrapperHeight(`${height ? height: accordionBody.scrollHeight}px`), 450)
+                !height && setHeigth(accordionBody.scrollHeight)
+                
+            } else {
+                setWrapperHeight(`auto`); 
+            }
+        };
+        
+        updateWrapperHeight();
+
+    }, [isExpanded]);
+    console.log(height)
     return (<>
             <div className={classNames(
                 styles.accordion,
                 {[styles.hidden]: isExpanded},
                     )}>
-                <div className={styles.accordionBody}>
-                    <div>
-                    <p className={styles.description}>
-                        {description}
-                    </p>
+                <div ref={accordionBodyRef} className={styles.accordionBody}>
+                    <div
+                    className={styles.accordionWrapper}
+                    style={{ height: wrapperHeight }}
+                    >
+                        <p className={styles.description}>
+                            {description}
+                        </p>
                     </div>
                 </div>
             <button className={styles.button} onClick={toggleDescription}>
